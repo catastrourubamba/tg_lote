ae7fe29c66b9aa86a6b032e3405a8cf2e6b2cc0a
catastrourubamba <caturu2022@gmail.com>
2022-10-28 10:09:38 -0500

    first commit

---
+++ data/validacion_tg_lote_1.js
@@ -0,0 +1,7 @@
+var json_validacion_tg_lote_1 = {
+"type": "FeatureCollection",
+"name": "tg_gejson",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+]
+}
